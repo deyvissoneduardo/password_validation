73bef7b4bbd0392168d94c79b99feca6b2efd8da
deyvissoneduardo <deyvissoneduardo22@gmail.com>
2025-04-01 17:40:31 -0300

    init project

---
+++ android/app/src/main/kotlin/br/com/passwor/validation/app/password_validation/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.passwor.validation.app.password_validation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:password_validation/src/password_validation_app.dart';
+
+void main() {
+  runApp(PasswordValidationApp());
+}
